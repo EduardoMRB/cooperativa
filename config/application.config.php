2cb8b1696520e067a95384c9d44150280f10e857
EduardoMRB <eduardomrb@gmail.com>
2013-09-13 02:42:21 -0400

    Adiciona zftool ao composer.json, adiciona novo module a config da aplicação e cria pastas de código fonte

--- config/application.config.php
+++ config/application.config.php
@@ -3,6 +3,7 @@ return array(
     // This should be an array of module namespaces used in the application.
     'modules' => array(
         'Application',
+        'Cooperativa',
     ),
 
     // These are various options for the listeners attached to the ModuleManager
